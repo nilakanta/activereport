c1be7f120611ab5e3d1e20bc6e91bc2fd402b3b0
Nilakanta Mallick <nila@activesphere.com>
2011-01-05 00:16:24 +0530

    Initial commit of unmodified new Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Activereport::Application.config.secret_token = '78ca306f30158d8bc0e95baa815da1e34d17d6e780f3fa7c6c9c09b640200b6b629020457e74f71ffe0e3b552fb2103139598dffe3c14c503d5dfb05616311b0'
